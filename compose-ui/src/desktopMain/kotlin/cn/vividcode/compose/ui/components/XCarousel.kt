5ebfa00baf0cd027c984f82376cf3b8a659e446c
李佳伟 <2965502228@qq.com>
2024-08-25 15:21:59 +0800

    修复：轮播图组件错误修复

--- compose-ui/src/desktopMain/kotlin/cn/vividcode/compose/ui/components/XCarousel.kt
+++ compose-ui/src/desktopMain/kotlin/cn/vividcode/compose/ui/components/XCarousel.kt
@@ -27,7 +27,8 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import cn.vividcode.compose.ui.components.XCarouselIndicatorType.Dot
 import cn.vividcode.compose.ui.components.XCarouselIndicatorType.Line
-import cn.vividcode.compose.ui.expends.timer
+import cn.vividcode.compose.ui.utils.timer
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 /**
@@ -40,6 +41,32 @@ import kotlinx.coroutines.launch
  * 文件介绍：轮播图
  */
 @Composable
+fun <T> XCarousel(
+	modifier: Modifier = Modifier.fillMaxSize(),
+	items: Array<T>,
+	firstIndex: Int = 0,
+	animationSpec: AnimationSpec<Float> = tween(durationMillis = 400),
+	button: XCarouselButton = XCarousels.button(),
+	indicator: XCarouselIndicator = XCarousels.indicator(),
+	autoScroller: XCarouselAutoScroller = XCarousels.autoScroller(),
+	content: @Composable LazyItemScope.(index: Int, item: T) -> Unit,
+) {
+	XCarousels(
+		modifier = modifier,
+		items = items.toList(),
+		firstIndex = firstIndex,
+		animationSpec = animationSpec,
+		button = button,
+		indicator = indicator,
+		autoScroller = autoScroller,
+		content = content,
+	)
+}
+
+/**
+ * 轮播图
+ */
+@Composable
 fun <T> XCarousels(
 	modifier: Modifier = Modifier.fillMaxSize(),
 	items: List<T>,
@@ -50,32 +77,34 @@ fun <T> XCarousels(
 	autoScroller: XCarouselAutoScroller = XCarousels.autoScroller(),
 	content: @Composable LazyItemScope.(index: Int, item: T) -> Unit,
 ) {
-	val listState = rememberLazyListState(firstIndex)
-	val widthState = remember { mutableIntStateOf(-1) }
-	val indexState = remember { mutableIntStateOf(firstIndex) }
-	val autoScrollerResetState = remember { mutableStateOf(false) }
-	val pageState by remember {
-		mutableStateOf(
-			value = PageState(indexState, items.size, listState, widthState, animationSpec)
-		)
-	}
-	if (autoScroller.enabled) {
-		LaunchedEffect(Unit) {
-			timer(
-				delay = 5000L,
-				reset = {
-					val reset = autoScrollerResetState.value
-					autoScrollerResetState.value = false
-					widthState.value == -1 || reset
-				}
-			) {
-				pageState.moveToNext()
-			}
-		}
-	}
 	Box(
 		modifier = modifier
 	) {
+		val listState = rememberLazyListState(firstIndex)
+		val widthState = remember { mutableIntStateOf(-1) }
+		val indexState = remember { mutableIntStateOf(firstIndex) }
+		val autoScrollerResetState = remember { mutableStateOf(false) }
+		val coroutineScope = rememberCoroutineScope()
+		val pageState by remember {
+			mutableStateOf(
+				PageState(indexState, items.size, listState, widthState, coroutineScope, animationSpec)
+			)
+		}
+		var offsetX by remember { mutableStateOf(0f) }
+		if (autoScroller.enabled) {
+			LaunchedEffect(Unit) {
+				timer(
+					delay = autoScroller.internal,
+					reset = {
+						val reset = autoScrollerResetState.value
+						autoScrollerResetState.value = false
+						widthState.value == -1 || reset
+					},
+				) {
+					pageState.moveToNext(this)
+				}
+			}
+		}
 		LazyRow(
 			modifier = Modifier
 				.fillMaxSize()
@@ -117,25 +146,169 @@ fun <T> XCarousels(
 	}
 }
 
-@Composable
-fun <T> XCarousel(
-	modifier: Modifier = Modifier.fillMaxSize(),
-	items: Array<T>,
-	firstIndex: Int = 0,
-	button: XCarouselButton = XCarousels.button(),
-	indicator: XCarouselIndicator = XCarousels.indicator(),
-	autoScroller: XCarouselAutoScroller = XCarousels.autoScroller(),
-	content: @Composable LazyItemScope.(index: Int, item: T) -> Unit,
+/**
+ * 页面状态
+ */
+private class PageState(
+	val indexState: MutableIntState,
+	private val count: Int,
+	private val listState: LazyListState,
+	private val widthState: MutableIntState,
+	private val coroutineScope: CoroutineScope,
+	private val animationSpec: AnimationSpec<Float>,
 ) {
-	XCarousels(
-		modifier = modifier,
-		items = items.toList(),
-		firstIndex = firstIndex,
-		button = button,
-		indicator = indicator,
-		autoScroller = autoScroller,
-		content = content,
-	)
+	
+	fun moveToNext(
+		coroutineScope: CoroutineScope = this.coroutineScope,
+	): Boolean {
+		if (listState.isScrollInProgress ||
+			listState.firstVisibleItemIndex >= count - 1 ||
+			indexState.value >= count - 1 ||
+			widthState.value == -1
+		) return false
+		coroutineScope.launch {
+			indexState.value++
+			val value = (listState.firstVisibleItemScrollOffset + widthState.value - 1) % widthState.value + 1
+			listState.animateScrollBy(value.toFloat(), animationSpec)
+		}
+		return true
+	}
+	
+	fun moveToLast(
+		coroutineScope: CoroutineScope = this.coroutineScope,
+	): Boolean {
+		if (listState.isScrollInProgress ||
+			listState.firstVisibleItemIndex <= 0 ||
+			indexState.value <= 0 &&
+			widthState.value == -1
+		) return false
+		coroutineScope.launch {
+			indexState.value--
+			val value = (listState.firstVisibleItemScrollOffset + widthState.value - 1) % widthState.value + 1
+			listState.animateScrollBy(-value.toFloat(), animationSpec)
+		}
+		return true
+	}
+}
+
+/**
+ * 轮播图按钮
+ */
+@Composable
+private fun BoxScope.XCarouselButtons(
+	pageState: PageState,
+	button: XCarouselButton,
+	autoScrollerResetState: MutableState<Boolean>,
+) {
+	XCarouselButton(
+		icon = Icons.AutoMirrored.Rounded.KeyboardArrowLeft,
+		align = button.alignment.leftAlign,
+		button = button
+	) {
+		if (pageState.moveToLast()) {
+			autoScrollerResetState.value = true
+		}
+	}
+	XCarouselButton(
+		align = button.alignment.rightAlign,
+		icon = Icons.AutoMirrored.Rounded.KeyboardArrowRight,
+		button = button
+	) {
+		if (pageState.moveToNext()) {
+			autoScrollerResetState.value = true
+		}
+	}
+}
+
+/**
+ * 轮播图按钮
+ */
+@Composable
+private fun BoxScope.XCarouselButton(
+	icon: ImageVector,
+	align: Alignment,
+	button: XCarouselButton,
+	onClick: () -> Unit,
+) {
+	Box(
+		modifier = Modifier
+			.padding(button.padding)
+			.size(52.dp)
+			.align(align)
+			.clip(button.shape)
+			.background(Color.Black.copy(alpha = 0.3f))
+			.clickable(onClick = onClick),
+		contentAlignment = Alignment.Center
+	) {
+		Icon(
+			imageVector = icon,
+			contentDescription = null,
+			modifier = Modifier.size(40.dp),
+			tint = Color.White.copy(alpha = 0.9f)
+		)
+	}
+}
+
+/**
+ * 轮播图指示器 - 点
+ */
+@Composable
+private fun BoxScope.XCarouselDotIndicator(
+	index: Int,
+	count: Int,
+	indicator: XCarouselIndicator,
+) {
+	Row(
+		modifier = Modifier
+			.align(Alignment.BottomCenter)
+			.padding(bottom = indicator.padding)
+	) {
+		repeat(count) {
+			Spacer(
+				modifier = Modifier
+					.padding(horizontal = 3.dp)
+					.size(8.dp)
+					.clip(CircleShape)
+					.background(
+						color = if (index == it) MaterialTheme.colorScheme.surface else Color.White.copy(alpha = 0.4f),
+					)
+			)
+		}
+	}
+}
+
+/**
+ * 轮播图指示器 - 线
+ */
+@Composable
+private fun BoxScope.XCarouselLineIndicator(
+	index: Int,
+	count: Int,
+	indicator: XCarouselIndicator,
+	horizontalPadding: Dp,
+) {
+	Row(
+		modifier = Modifier
+			.fillMaxWidth()
+			.align(Alignment.BottomCenter)
+			.padding(
+				horizontal = indicator.padding + horizontalPadding,
+				vertical = indicator.padding
+			),
+	) {
+		repeat(count) {
+			Spacer(
+				modifier = Modifier
+					.weight(1f)
+					.height(3.dp)
+					.padding(horizontal = 4.dp)
+					.clip(RoundedCornerShape(1.dp))
+					.background(
+						color = if (index == it) MaterialTheme.colorScheme.surface else Color.White.copy(alpha = 0.4f),
+					)
+			)
+		}
+	}
 }
 
 /**
@@ -163,173 +336,20 @@ object XCarousels {
 	): XCarouselIndicator = XCarouselIndicator(enabled, type, padding)
 	
 	/**
-	 * 自动轮播
+	 * 自动滚动
 	 */
 	fun autoScroller(
 		enabled: Boolean = true,
 		internal: Long = 5000L,
 	): XCarouselAutoScroller = XCarouselAutoScroller(enabled, internal)
-}
-
-/**
- * 页面状态
- */
-private class PageState(
-	val indexState: MutableIntState,
-	private val count: Int,
-	private val listState: LazyListState,
-	private val widthState: MutableIntState,
-	private val animationSpec: AnimationSpec<Float>,
-) {
 	
-	suspend fun moveToNext(): Boolean {
-		if (listState.isScrollInProgress ||
-			listState.firstVisibleItemIndex >= count - 1 ||
-			indexState.value >= count - 1 ||
-			widthState.value == -1
-		) return false
-		indexState.value++
-		listState.animateScrollBy(widthState.value.toFloat(), animationSpec)
-		return true
-	}
-	
-	suspend fun moveToLast(): Boolean {
-		if (listState.isScrollInProgress ||
-			listState.firstVisibleItemIndex <= 0 ||
-			indexState.value <= 0 &&
-			widthState.value == -1
-		) return false
-		indexState.value--
-		listState.animateScrollBy(-widthState.value.toFloat(), animationSpec)
-		return true
-	}
-}
-
-/**
- * 轮播图按钮
- */
-@Composable
-private fun BoxScope.XCarouselButtons(
-	pageState: PageState,
-	button: XCarouselButton,
-	autoScrollerResetState: MutableState<Boolean>,
-) {
-	val coroutineScope = rememberCoroutineScope()
-	XCarouselButton(
-		align = button.alignment.leftAlign,
-		icon = Icons.AutoMirrored.Rounded.KeyboardArrowLeft,
-		padding = button.padding,
-	) {
-		coroutineScope.launch {
-			val move = pageState.moveToLast()
-			if (move) {
-				autoScrollerResetState.value = true
-			}
-		}
-	}
-	XCarouselButton(
-		align = button.alignment.rightAlign,
-		icon = Icons.AutoMirrored.Rounded.KeyboardArrowRight,
-		padding = button.padding,
-	) {
-		coroutineScope.launch {
-			val move = pageState.moveToNext()
-			if (move) {
-				autoScrollerResetState.value = true
-			}
-		}
-	}
-}
-
-/**
- * 轮播图按钮
- */
-@Composable
-private fun BoxScope.XCarouselButton(
-	align: Alignment,
-	icon: ImageVector,
-	padding: PaddingValues,
-	onClick: () -> Unit,
-) {
-	Box(
-		modifier = Modifier
-			.padding(padding)
-			.size(52.dp)
-			.align(align)
-			.clip(RoundedCornerShape(10.dp))
-			.background(Color.Black.copy(alpha = 0.3F))
-			.clickable(onClick = onClick),
-		contentAlignment = Alignment.Center
-	) {
-		Icon(
-			imageVector = icon,
-			contentDescription = null,
-			modifier = Modifier.size(40.dp),
-			tint = Color.White.copy(alpha = 0.9F)
-		)
-	}
-}
-
-/**
- * 轮播图指示器 - 点
- */
-@Composable
-private fun BoxScope.XCarouselDotIndicator(
-	index: Int,
-	count: Int,
-	indicator: XCarouselIndicator,
-) {
-	Row(
-		modifier = Modifier
-			.align(Alignment.BottomCenter)
-			.padding(bottom = indicator.bottomPadding)
-	) {
-		repeat(count) {
-			Spacer(
-				modifier = Modifier
-					.padding(horizontal = 3.dp)
-					.size(9.dp)
-					.clip(CircleShape)
-					.background(
-						color = if (index == it) MaterialTheme.colorScheme.surface else Color.White.copy(alpha = 0.3f),
-					)
-			)
-		}
-	}
-}
-
-/**
- * 轮播图指示器 - 线
- */
-@Composable
-private fun BoxScope.XCarouselLineIndicator(
-	index: Int,
-	count: Int,
-	indicator: XCarouselIndicator,
-	horizontalPadding: Dp,
-) {
-	Row(
-		modifier = Modifier
-			.fillMaxWidth()
-			.align(Alignment.BottomCenter)
-			.padding(
-				horizontal = 12.dp + horizontalPadding,
-				vertical = indicator.bottomPadding
-			),
-	) {
-		repeat(count) {
-			Spacer(
-				modifier = Modifier
-					.weight(1F)
-					.height(3.dp)
-					.padding(horizontal = 6.dp)
-					.clip(CircleShape)
-					.background(
-						color = if (index == it) MaterialTheme.colorScheme.surface else Color.White.copy(alpha = 0.3f),
-					)
-			)
-		}
-	}
+	/**
+	 * 手势轮播
+	 */
+	fun userScroller(
+		enabled: Boolean = true,
+		critical: Dp = 20.dp,
+	): XCarouselUserScroller = XCarouselUserScroller(enabled, critical)
 }
 
 /**
@@ -363,15 +383,7 @@ enum class XCarouselButtonAlignment(
 data class XCarouselIndicator internal constructor(
 	val enabled: Boolean,
 	val type: XCarouselIndicatorType,
-	val bottomPadding: Dp,
-)
-
-/**
- * 轮播图自动轮播
- */
-data class XCarouselAutoScroller internal constructor(
-	val enabled: Boolean,
-	val internal: Long,
+	val padding: Dp,
 )
 
 /**
@@ -383,3 +395,19 @@ enum class XCarouselIndicatorType {
 	
 	Line,
 }
+
+/**
+ * 轮播图自动轮播
+ */
+data class XCarouselAutoScroller internal constructor(
+	val enabled: Boolean,
+	val internal: Long,
+)
+
+/**
+ * 轮播图用户手势
+ */
+data class XCarouselUserScroller internal constructor(
+	val enabled: Boolean,
+	val critical: Dp,
+)
